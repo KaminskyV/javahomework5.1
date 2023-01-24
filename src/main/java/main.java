408315f7ae8da9f7ac23341a8b04deb468ecbeeb
Kaminsky <kamwlad2010@gmail.com>
2023-01-24 20:15:00 +0400

    main

---
+++ src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int bonus = (int) service.calculate(5000,true);
+        System.out.println(bonus);
+    }
+}
